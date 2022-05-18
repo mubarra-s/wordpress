f995bd69ac66c2f314c8f393780bf0f7e009cff3
mubarra-s <105634706+mubarra-s@users.noreply.github.com>
2022-05-18 04:29:53 -0700

    Create Texonomy.php

---
+++ Texonomy.php
@@ -0,0 +1,32 @@
+<?php get_header(); // Footer ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ 
+taxonomy-course.php
+
+<h3><?php echo '<a href="'.get_the_permalink().'">'; the_title(); echo '</a>'; ?></h3>
+ 
+<?php the_content(); ?>
+<?php wp_link_pages(); ?>
+<?php edit_post_link(); ?>
+ 
+<?php endwhile; ?>
+ 
+<?php
+if ( get_next_posts_link() ) {
+next_posts_link();
+}
+?>
+<?php
+if ( get_previous_posts_link() ) {
+previous_posts_link();
+}
+?>
+ 
+<?php else: ?>
+ 
+<p>No posts found. :(</p>
+ 
+<?php endif; ?>
+
+<?php get_footer(); // Footer ?>
